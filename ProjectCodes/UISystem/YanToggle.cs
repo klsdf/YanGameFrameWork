5078cfb186e062baa6da27cefea8cebc7ff4a1bc
闫辰祥 <1796655849@qq.com>
2025-04-17 18:00:45 +0800

    增加toggle的注释

--- ProjectCodes/UISystem/YanToggle.cs
+++ ProjectCodes/UISystem/YanToggle.cs
@@ -1,7 +1,13 @@
+/****************************************************************************
+ * Author: 闫辰祥
+ * Date: 2025-04-17
+ * Description: 一个可以切换状态的按钮，需要来手动继承这个类。
+ * 需要设置groupName，只有同一个groupName的按钮才会互相影响。也就是说按下一个的时候，其他同一个groupName的按钮会自动关闭。
+ ****************************************************************************/
 using UnityEngine.EventSystems;
 using UnityEngine;
 using System;
-public abstract class YanToggle : MonoBehaviour,IPointerClickHandler
+public abstract class YanToggle : MonoBehaviour, IPointerClickHandler
 {
     public string groupName;
 
@@ -13,30 +19,28 @@ public abstract class YanToggle : MonoBehaviour,IPointerClickHandler
     protected virtual void Start()
     {
 
-        YanGF.Event.AddListener<string,string>("OnButtonClick",  OnButtonClick);
+        YanGF.Event.AddListener<string, string>("OnButtonClick", OnButtonClick);
     }
 
     protected virtual void OnDestroy()
     {
-        YanGF.Event.RemoveListener<string,string>("OnButtonClick",  OnButtonClick);
+        YanGF.Event.RemoveListener<string, string>("OnButtonClick", OnButtonClick);
     }
 
 
 
-    public virtual void OnPointerClick(PointerEventData eventData)
+    public void OnPointerClick(PointerEventData eventData)
     {
-        YanGF.Event.TriggerEvent<string,string>("OnButtonClick", groupName,name);
-
-
+        YanGF.Event.TriggerEvent<string, string>("OnButtonClick", groupName, name);
     }
 
 
 
-    private void OnButtonClick(string groupName,string name)
+    private void OnButtonClick(string groupName, string name)
     {
 
         //如果不是自己的组，则不响应
-        if(groupName!=this.groupName)
+        if (groupName != this.groupName)
         {
             return;
         }
@@ -44,32 +48,48 @@ public abstract class YanToggle : MonoBehaviour,IPointerClickHandler
         if (name == this.name)
         {
             TurnOn();
-        }else{
+        }
+        else
+        {
             TurnOff();
         }
     }
 
 
 
-    public Action OnClickSelf;
-    public Action OnClickOther;
 
 
 
 
+    //////////////////////////////////////////公共方法//////////////////////////////////////////
 
+
+    /// <summary>
+    /// 打开按钮，同时会关闭其他同一个groupName的按钮
+    /// </summary>
     public void TurnOn()
     {
         isActive = true;
         OnClickSelf();
     }
 
+    /// <summary>
+    /// 关闭按钮
+    /// </summary>
     public void TurnOff()
     {
         isActive = false;
         OnClickOther();
     }
 
+    /// <summary>
+    /// 点击自身按钮时触发的事件
+    /// </summary>
+    public Action OnClickSelf;
+    /// <summary>
+    /// 点击其他按钮时触发的事件
+    /// </summary>
+    public Action OnClickOther;
 
 
 
